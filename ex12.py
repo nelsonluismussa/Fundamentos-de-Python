71b64580e4282a4a099d74e1005c7227267e44e5
Nelson L.Mussa <nelsonluismussa@gmail.com>
2024-10-29 08:11:05 +0200

    aula

---
+++ ex12.py
@@ -0,0 +1,8 @@
+comida = input('Entre com nome da comida (# pra sair): ')
+
+while not comida == 'q':
+    print(f'Voce gosta {comida}')
+    comida = input('Entre com nome da comida (# pra sair): ')
+
+
+print('Bye')
